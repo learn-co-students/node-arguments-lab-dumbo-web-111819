b77a39ea1c4d10f6bf02d862bdb0fc36230ca764
ajshemi <akpojotor@yahoo.com>
2020-04-22 23:50:03 -0400

    Done.

--- password.js
+++ password.js
@@ -1,1 +1,24 @@
-// code solution here
+let _ = require('lodash');
+
+process.argv.shift()
+process.argv.shift()
+passwordLength=parseInt(process.argv[0])
+noOfPassword=parseInt(process.argv[1])
+
+function generatePassword(){
+    let emptyString=''
+    let passWord="abcdefghijklmnopqrstuvwxyz123456789"
+    let i=0
+    while(i!==passwordLength){
+        emptyString+=_.sample(passWord.split(''))
+        i++
+    }
+    return emptyString
+}
+
+let j=0
+while (j!==noOfPassword){
+    process.stdout.write(`${generatePassword()}\n`)
+    j++
+}
+process.exit(0)
